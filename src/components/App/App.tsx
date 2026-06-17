c0c5e167ba0583bb7b5ecc8721157e73484d1355
Anastasiia <anastasiia@Anastasiia-MacBook-Air.local>
2026-06-17 15:51:39 +0300

    Done

---
+++ src/components/App/App.tsx
@@ -0,0 +1,47 @@
+import css from "./App.module.css"
+import CafeInfo from "../CafeInfo/CafeInfo.tsx";
+import VoteOptions from "../VoteOptions/VoteOptions.tsx";
+import {useState} from "react";
+
+import VoteStats from "../VoteStats/VoteStats.tsx";
+import Notification from "../Notification/Notification.tsx";
+import type {Votes, VoteType} from "../../types/votes.ts";
+
+export default function App() {
+    const initialState: Votes = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+    }
+    const [votes, setVotes] = useState(initialState);
+
+    const handleVote = (type: VoteType): void => {
+        setVotes((prevState: Votes) => ({
+            ...prevState,
+            [type]: prevState[type] + 1,
+        }));
+    };
+
+    const resetVotes = (): void => {
+        setVotes(initialState);
+    }
+const totalVotes: number = votes.good + votes.neutral + votes.bad;
+    const positiveRate: number = totalVotes
+        ? Math.round((votes.good / totalVotes) * 100)
+        : 0;
+
+    return (
+        <div className={css.app}>
+            <div>
+                <CafeInfo/>
+                <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0}/>
+
+                {totalVotes > 0
+                    ? <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate}/>
+                    : <Notification/>
+                }
+
+            </div>
+        </div>
+    );
+}
